d17db9e5ab25da51aa3408ecb27ed3c1078eb45a
Koncz Norbert <111992880+Norbano@users.noreply.github.com>
2025-07-18 11:15:27 +0000

    new function added

--- proba.c
+++ proba.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char* argc[]) {
+
+void peldaFg(){
+    printf("Hello, World!\n");
+}
+
+
+int main(int argc, char* argv[]) {
 
     return 0;
 }
+
+// This is a simple C program that prints "Hello, World!" to the console.
